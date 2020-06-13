6bf81b9ce4404b03fd7c68494f54a0d514f32a24
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2020-06-13 11:00:48 -0300

    Add Status Bar, App provider and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import AppProvider from './hooks';
 
-const App: React.FC = () => {
-  return <View />;
-};
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#f3903d" />
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#f3903d' }}>
+        <Routes />
+      </View>
+    </AppProvider>
+  </NavigationContainer>
+);
 
 export default App;
